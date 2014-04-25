805159f2ca65c00f851c7c84bc0ee688000f05e5
Mel Zhang <zeemelon@gmail.com>
2014-04-25 06:34:18 -0700

    now YT url is validated for uniqueness

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,7 @@
 class Video < ActiveRecord::Base
 
   # validates_format_of :url, :with => /\Ahttps:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_&#]*)\z/
-
+validates_uniqueness_of :url
 
 # scopes
   scope :sorted, lambda { order("videos.position ASC") }
