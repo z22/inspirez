46bd3d58b611207517e81b46baf9219fe8aa0fb0
Mel Zhang <zeemelon@gmail.com>
2014-04-29 18:35:26 -0700

    rails_admin u/p

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -37,7 +37,7 @@ RailsAdmin.config do |config|
   RailsAdmin.config do |config|
   config.authorize_with do
     authenticate_or_request_with_http_basic('Site Message') do |username, password|
-      username == 'foo' && password == 'bar'
+      username == 'm' && password == 'z'
     end
   end
 
